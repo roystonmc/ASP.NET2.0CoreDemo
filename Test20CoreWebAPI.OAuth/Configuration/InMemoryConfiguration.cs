e0e4f90df070debb7acddec8f8c6df34f1d038a9
rmcneill <rmcneill@gmail.com>
2018-01-15 00:29:23 -0500

    Initial check-in of module ASPNetCore2.0Sol

---
+++ Test20CoreWebAPI.OAuth/Configuration/InMemoryConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace Test20CoreWebAPI.OAuth.Configuration
+{
+    public class InMemoryConfiguration
+    {
+        public InMemoryConfiguration()
+        {
+        }
+
+        public static IEnumerable<ApiResource> ApiResources()
+        {
+            return new[] {
+                new ApiResource("socialNetwork", "Social Network")
+
+            };
+        }
+
+        public static IEnumerable<Client> Clients()
+        {
+            return new[] {
+
+                new Client{
+                    ClientId = "socialNetwork",
+                    ClientSecrets = new [] { new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new [] {"socialNetwork"}
+                }
+            };
+        }
+
+        public static IEnumerable<TestUser> Users()
+        {
+            return new[] {
+
+                new TestUser {
+                    SubjectId = "1",
+                    Username = "rmcneill@gmail.com",
+                    Password = "password"
+                }
+            };
+        }
+    }
+}
